1c1cc7e9c1cd4b84becd9599ef4e66afa3c7007e
abdessamad najdi <abdessamadnajdi@abdessamads-mbp>
2016-11-13 15:28:25 +0000

    Initial Commit

---
+++ pageScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageScroll
+//
+//  Created by abdessamad najdi on 13/11/2016.
+//  Copyright © 2016 najdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
